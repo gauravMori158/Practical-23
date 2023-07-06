662fb7e3d814b64ca053bb37b57c8170ddad8fbd
Gaurav Mori <gkmori444@gmail.com>
2023-07-06 16:55:54 +0530

    Factory Design Pattern

---
+++ Factory Design Pattern/Controllers/EmployeeController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Factory_Design_Pattern.Models;
+
+namespace Factory_Design_Pattern.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly BAL dal;
+        public EmployeeController(BAL bAL )
+        {
+                dal = bAL;
+        }
+
+        
+        [HttpGet]
+        [Route("EmployeeList")]
+        public IActionResult Get(int? id)
+        {
+            if (id.HasValue)
+            {
+                var employee = dal.GetEmployeeById(id.Value);
+                return Ok(employee);
+            }
+            else
+            {
+                var employees = dal.GetEmployees();
+                return Ok(employees);
+            }
+
+        }
+
+        [HttpPost]
+        [Route("CreateEmployee")]
+        public IActionResult Create(Employee employee)
+        {
+            dal.CreateEmployee(employee);
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("UpdateEmployee")]
+        public IActionResult Update(int id, Employee employee)
+        {
+            if (id != employee.Id)
+                return BadRequest();
+
+
+            dal.UpdateEmployee(employee);
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("Get OT")]
+        public IActionResult OT(int Hourse, int employeeId)
+        {
+            
+            return Ok(dal.GetOverTime(Hourse, employeeId));
+        }
+
+        [HttpDelete]
+        [Route("DeleteEmployee")]
+        public IActionResult Delete(int id)
+        {
+            dal.DeleteEmployee(id);
+            return Ok();
+        }
+    }
+}
---
+++ Factory Design Pattern/Models/BAL.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Factory_Design_Pattern.Models
+{
+    public class BAL
+    {
+        private readonly EmployeeDbContext context;
+        public BAL(EmployeeDbContext cont)
+        {
+                context= cont;
+        }
+        public void CreateEmployee(Employee employee)
+        {
+           
+                context.Employees.Add(employee);
+                context.SaveChanges();
+             
+        }
+
+        public int GetOverTime (int hours , int EmpId)
+        {
+            var employee = context.Employees.Include(e => e.Department); 
+            
+            int Ot= employee.FirstOrDefault(e=>e.Id == EmpId).Department.DepartmentPay * hours;
+            return Ot;
+        }
+        public List<Employee> GetEmployees()
+        {
+              var employees = context.Employees.Where(e=>e.Status == true).ToList();
+                return employees;
+            
+            
+        }
+        public  Employee GetEmployeeById(int id)
+        {
+             
+                return context.Employees.Where(e=>e.Status==true).FirstOrDefault(e=>e.Id == id);
+             
+        }
+        public void DeleteEmployee(int id)
+        {
+             
+                var employee = context.Employees.Find(id);
+                employee.Status = false;
+                //context.Employees.Remove(employee);
+                context.SaveChanges();
+            
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            
+                context.Employees.Update(employee);
+                context.SaveChanges();
+
+             
+        }
+
+    }
+}
